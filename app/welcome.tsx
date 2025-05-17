8f1896f006914ee45fa8be1e36b4502aeeef3cba
Shawn Law <shawnlaw6669@gmail.com>
2025-05-17 12:09:19 +0800

    display images..some progress...

--- app/welcome.tsx
+++ app/welcome.tsx
@@ -1,11 +1,53 @@
-import { View, Text, TouchableOpacity } from 'react-native'
+import { View, Text, TouchableOpacity, Image, ScrollView } from 'react-native'
 import { SafeAreaView } from 'react-native-safe-area-context'
+
+const events = [
+  {
+    id: 1,
+    image: require('../assets/images/1.jpg'),
+  },
+  {
+    id: 2,
+    image: require('../assets/images/2.jpg'),
+  },
+  {
+    id: 3,
+    image: require('../assets/images/3.jpg'),
+  },
+  {
+    id: 4,
+    image: require('../assets/images/4.jpg'),
+  },
+  {
+    id: 5,
+    image: require('../assets/images/5.jpg'),
+  },
+  {
+    id: 6,
+    image: require('../assets/images/6.jpg'),
+  },
+  {
+    id: 7,
+    image: require('../assets/images/7.jpg'),
+  },
+  {
+    id: 8,
+    image: require('../assets/images/8.jpg'),
+  },
+]
 
 export default function welcome() {
   return (
     <SafeAreaView className='bg-yellow-950 flex-1'>
-      <View className='bg-gray-400 h-[60%] w-full'>
+      <View className='h-[60%] w-full'>
         {/* Marquee component */}
+        <ScrollView horizontal>
+          {events.map((event) => (
+            <View className='h-full w-96 p-5' key={event.id}>
+              <Image source={event.image} className='h-full w-full rounded-3xl'/>
+            </View>
+          ))}
+        </ScrollView>
       </View>
 
       <View className='gap-4 p-4 flex-1 justify-center'>
